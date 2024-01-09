172ccbc7e66d6736e7dbdd3ad3a5b742d6dc80e9
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-01-09 21:44:38 +0300

    Create wild.js

---
+++ jx-main/js/wild.js
@@ -0,0 +1,16 @@
+!function(a) {
+    var b = {
+        total: 10,
+        ofTop: 0,
+        ofLeft: 0,
+        on: "document.body",
+        twinkle: .2,
+        minPixel: 1,
+        maxPixel: 2,
+        color: "#fff",
+        namespace: "jqueryFireFly",
+        zIndex: Math.ceil(20 * Math.random()) - 1,
+        borderRadius: "50%",
+        _paused: !1
+    };
+  }(jQuery);
